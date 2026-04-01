2c38284b9ea1247ac6f4c52c96b6c7a042d66b3d
Magali MAI <maimagali26.pro@gmail.com>
2026-04-01 21:45:44 +0200

    Add cross plateform local network dev environment

--- frontend/src/modules/shared/components/Navbar/index.tsx
+++ frontend/src/modules/shared/components/Navbar/index.tsx
@@ -1,8 +1,5 @@
+import styles from "./Navbar.module.scss";
 
-export default function Navbar(){
-  return (
-    <header>
-      Navbar
-    </header>
-  );
-};
+export default function Navbar() {
+  return <header className={styles.navbar}>Navbar</header>;
+}
