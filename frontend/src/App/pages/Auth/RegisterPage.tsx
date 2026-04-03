43c0055b6202bf781ebbcf570bf28a91c4955ce6
Magali MAI <maimagali26.pro@gmail.com>
2026-04-03 09:06:51 +0200

    Rectife title

--- frontend/src/App/pages/Auth/RegisterPage.tsx
+++ frontend/src/App/pages/Auth/RegisterPage.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@modules/auth/components/molecules/RegisterForm";
 export default function RegisterPage() {
   return (
     <>
-      <h1>Register page</h1>
+      <h1>Créer un compte</h1>
       <RegisterForm />
     </>
   );
